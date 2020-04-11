61402f1f2848254bd702f2d45a129e6934c65ca4
Mark Murnane <mark@hackafe.net>
2020-04-11 02:28:57 -0400

    Making wget  quiet

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -4,7 +4,7 @@ sudo apt install coreutils p7zip-full qemu-user-static
 git clone https://github.com/bitbyt3r/CustomPiOS.git
 git clone https://github.com/bitbyt3r/FullPageOS.git
 cd FullPageOS/src/image
-wget -c --trust-server-names 'https://downloads.raspberrypi.org/raspbian_lite_latest'
+wget -q -c --trust-server-names 'https://downloads.raspberrypi.org/raspbian_lite_latest'
 cd ..
 ../../CustomPiOS/src/update-custompios-paths
 sudo modprobe loop
