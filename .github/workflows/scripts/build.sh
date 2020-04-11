41c4983eab39bd25ec9fc8adb49ad0e64f8f2bf3
Mark Murnane <mark@hackafe.net>
2020-04-11 02:22:10 -0400

    Changed to forked repos

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt install coreutils p7zip-full qemu-user-static
 
-git clone https://github.com/guysoft/CustomPiOS.git
-git clone https://github.com/guysoft/FullPageOS.git
+git clone https://github.com/bitbyt3r/CustomPiOS.git
+git clone https://github.com/bitbyt3r/FullPageOS.git
 cd FullPageOS/src/image
 wget -c --trust-server-names 'https://downloads.raspberrypi.org/raspbian_lite_latest'
 cd ..
